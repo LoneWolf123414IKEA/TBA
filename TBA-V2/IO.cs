e059edeef72bc93c20c7cf456780eb2ed684f733
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2023-03-17 09:56:20 +0100

    Update IO.cs

--- TBA-V2/IO.cs
+++ TBA-V2/IO.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,78 +10,135 @@ namespace TBA_V2
 {
     internal class IO
     {
-        private static int outint;
-        private static string outtxt;
-        private static int cursororig;
+        private static ConsoleColor MapFore = ConsoleColor.White;
+        private static ConsoleColor MapBack = ConsoleColor.Black;
+        private static ConsoleColor MapLoot = ConsoleColor.Yellow;
+        private static ConsoleColor ChrFull = ConsoleColor.White;
+        private static ConsoleColor ChrHalf = ConsoleColor.Red;
+        private static ConsoleColor ChrCrit = ConsoleColor.DarkRed;
+        private static ConsoleColor ChrBack = ConsoleColor.Black;
+        private static ConsoleColor GenFore = ConsoleColor.DarkGray;
+        private static ConsoleColor GenBack = ConsoleColor.Gray;
+        public static void Refresh(string type)
+        {
+            switch (type)
+            {
+                case "I":
+                    break;
+                default:
+                    Console.Clear();
+                    Console.Write("█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████" +
+                              "\r\n████▌                ▐███████████████████████████████████████████████████████████████████████████████████████████████████████████████████" +
+                              "\r\n█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████" +
+                              "\r\n█▌╔════════════════════════════════════════════════════════════════════════════════════════╗▐██▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀████████████████████" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████████████████" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████████████████" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████►HE◄▌  ▐▌ ▐█" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████►TO◄▌  ▐▌ ▐█" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████►LA◄▌  ▐▌ ▐█" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████►RA◄▌  ▐▌ ▐█" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████►LH◄▌  ▐▌ ▐█" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████►RH◄▌  ▐▌ ▐█" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████►LL◄▌  ▐▌ ▐█" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████►RL◄▌  ▐▌ ▐█" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████►LF◄▌  ▐▌ ▐█" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████►RF◄▌  ▐▌ ▐█" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████████████████" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████████████████" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████████████████" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████████████████" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████████████████" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████████████████" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████████████████" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████████████████" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████████████████" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████████████████" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████████████████" +
+                              "\r\n█▌║                                                                                        ║▐█▌                      ▐███████████████████" +
+                              "\r\n█▌║                                                                                        ║▐██▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄████████████████████" +
+                              "\r\n█▌║                                                                                        ║▐████████████████████████████████████████████" +
+                              "\r\n█▌║                                                                                        ║▐████████████████████████████████████████████" +
+                              "\r\n█▌╚════════════════════════════════════════════════════════════════════════════════════════╝▐██████                      ████████████████" +
+                              "\r\n███████████████████████████████████████████████████████████████████████████████████████████████████                      ██████►X◄▌   ▐██" +
+                              "\r\n█████████████████████████████████████████████████████████████████████████████████ ▌████▌██▌████▌███                      ████████████████" +
+                              "\r\n███████▌main hand▐▌                ▐█████████████████████████████████████████████ █▐████▐█▐████▐███                      ██████►Y◄▌   ▐██" +
+                              "\r\n█████████████████████████████████████████████████████████████████████████████████ █▌████▌▌████▌████                      ████████████████" +
+                              "\r\n███████▌off  hand▐▌                ▐█████████████████████████████████████████████ ██▐████▐████▐████                      ██████►Z◄▌≡ ≡▐██" +
+                              "\r\n█████████████████████████████████████████████████████████████████████████████████ ██▌███▌▌███▌█████                      ████████████████" +
+                              "\r\n█████████████████████████████████████████████████████████████████████████████████ ███▐██▐█▐██▐█████                      ██████►L◄▌≡ ≡▐██" +
+                              "\r\n█████████████████████████████████████████████████████████████████████████████████ ███▌█▌██▌█▌██████                      ██████►M◄▌≡ ≡▐██" +
+                              "\r\n█████████████████████████████████████████████████████████████████████████████████ ████ ▐███ ▐██████                      ██████►D◄▌≡ ≡▐██" +
+                              "\r\n█████████████████████████████████████████████████████████████████████████████████ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█                      ████████████████" +
+                              "\r\n█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████");
+                    break;
+            }
+        }
+        public static void Chrmodl(string part)
+        {
+            switch (part)
+            {
+                case "H":
+                    if (Program.player.hp <= 0) return;
+                    else if (Program.player.hp > 50) Console.ForegroundColor = ChrFull;
+                    else if ((Program.player.hp < 50) && (Program.player.hp > 15)) Console.ForegroundColor = ChrHalf;
+                    else Console.ForegroundColor = ChrCrit;
+                    Console.SetCursorPosition(96, 5);
+                    Console.Write("        ▄████▄        ");
+                    Console.SetCursorPosition(96, 6);
+                    Console.Write("       ████████       ");
+                    Console.SetCursorPosition(96, 7);
+                    Console.Write("      ▐████████▌      ");
+                    Console.SetCursorPosition(96, 7);
+                    Console.Write("       ████████       ");
+                    Console.SetCursorPosition(96, 7);
+                    Console.Write("       ▀██████▀       ");
+                    break;
+                case "T":
+                    break;
+                case "La":
+                    break;
+                case "Ra":
+                    break;
+                case "Lh":
+                    break;
+                case "Rh":
+                    break;
+                case "Ll":
+                    break;
+                case "Rl":
+                    break;
+                case "Lf":
+                    break;
+                case "Rf":
+                    break;
+                default:
+                    break;
+            }
+        }
         public static string IOtxt(string in1, string in2)
         {
-            Console.Write(in1);
-            outtxt = Console.ReadLine();
-            Console.SetCursorPosition(in1.Length + outtxt.Length, 0);
-            Console.Write(in2);
-            return outtxt;
+            return "";
         }
         public static string IOtxt(string in1)
         {
-            cursororig = Console.CursorLeft;
-            Console.Write(in1);
-            outtxt = Console.ReadLine();
-            Console.SetCursorPosition((cursororig + outtxt.Length), (Console.CursorTop - 1));
-            return outtxt;
+            return "";
+
         }
         public static int IOint(string in1, string in2)
         {
-            Console.Write(in1);
-            while (true)
-            {
-                cursororig = Console.CursorLeft;
-                try
-                {
-                    outint = int.Parse(Console.ReadLine());
-                    break;
-                }
-                catch
-                {
-                    Console.Write("INCORRECT INPUT");
-                    Console.Read();
-                    Console.SetCursorPosition((cursororig), (Console.CursorTop - 1));
-                }
-            }
-            Console.SetCursorPosition(in1.Length + outint.ToString().Length, 0);
-            Console.WriteLine(in2);
-            return outint;
+            return 0;
         }
         public static void O(string in1)
         {
-            Console.Write(in1);
+            
         }
         public static string Itxt()
         {
-            cursororig = Console.CursorLeft;
-            outtxt = Console.ReadLine();
-
-            Console.SetCursorPosition((cursororig + outtxt.Length), (Console.CursorTop - 1));
-            return outtxt;
+            return "";
         }
         public static int Iint()
         {
-            while (true)
-            {
-                cursororig = Console.CursorLeft;
-                try
-                {
-                    outint = int.Parse(Console.ReadLine());
-                    break;
-                }
-                catch
-                {
-                    Console.Write("INCORRECT INPUT");
-                    Console.Read();
-                    Console.SetCursorPosition((cursororig), (Console.CursorTop - 1));
-                }
-            }
-            Console.SetCursorPosition((cursororig + outint.ToString().Length), (Console.CursorTop - 1));
-            return outint;
+            return 0;
         }
     }
 }
