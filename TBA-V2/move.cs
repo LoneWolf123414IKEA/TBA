de9aadf2c635f83c5b3d4adb95648e6c39610fdb
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2023-03-18 01:15:46 +0100

    Update move.cs

--- TBA-V2/move.cs
+++ TBA-V2/move.cs
@@ -11,11 +11,12 @@ namespace TBA_V2
     internal class move
     {
         private rand rand = new rand();
-        public int[,,] map = new int[260, 260, 4];
+        public int[,,,] map = new int[260, 260, 4, 5];
         /*
         first indicats x coordinate
         second indicates y coordinate
-        third indicates function
+        third indicates z coordinate
+        fourth indicates function
             for pos 0:
                 0 = special
                 1 = monster
@@ -34,10 +35,13 @@ namespace TBA_V2
                 this only indicates lootability, 0 is no loot and anytitng above is amount of times
             for pos 3:
                 visited or not, 0 or a 1
+            for pos 4:
+                stairroom, 0 no, 1 down, 2 up
             
         */
         public int x;
         public int y;
+        public int z = 0;
         private int lastx;
         private int lasty;
         public void mapgen(string name)
@@ -48,40 +52,73 @@ namespace TBA_V2
                 {
                     for (int x1 = 0; x1 < 260; x1++)
                     {
-                        map[x1, y1, 0] = rand.wh(new int[] { 1, 90 });
-                        if (map[x1, y1, 0] == 0) map[x1, y1, 1] = 1;
-                        else map[x1, y1, 1] = rand.wh(new int[] { 9, 1 });
-                        map[x1, y1, 2] = rand.wh(new int[] { 10, 35, 4, 1 });
-
-                        map[x1, y1, 3] = 0;
-
-                        
+                        map[x1, y1, 0, 0] = rand.wh(new int[] { 1, 90 });
+                        if (map[x1, y1, 0, 0] == 0) map[x1, y1, 0, 1] = 1;
+                        else map[x1, y1, 0, 1] = rand.wh(new int[] { 9, 1 });
+                        map[x1, y1, 0, 2] = rand.wh(new int[] { 10, 35, 4, 1 });
+                        map[x1, y1, 0, 3] = 0;
                     }
                 }
-                map[0, 0, 1] = 1;
-                map[0, 0, 0] = 0;
-                map[0, 0, 2] = 1;
-                map[0, 0, 3] = 1;
+                map[0, 0, 0, 1] = 1;
+                map[0, 0, 0, 0] = 0;
+                map[0, 0, 0, 2] = 1;
+                map[0, 0, 0, 3] = 1;
 
-                map[255, 1, 0] = 0;
-                map[255, 1, 1] = 0;
-                map[255, 1, 2] = 0;
+                for (int y1 = 0; y1 < 260; y1++)
+                {
+                    for (int x1 = 0; x1 < 260; x1++)
+                    {
+                        map[x1, y1, 1, 0] = rand.wh(new int[] { 1, 90 });
+                        if (map[x1, y1, 1, 0] == 0) map[x1, y1, 1, 1] = 1;
+                        else map[x1, y1, 1, 1] = rand.wh(new int[] { 1, 6, 3 });
+                        map[x1, y1, 1, 2] = rand.wh(new int[] { 2, 5, 10, 35, 4, 1 });
+                        map[x1, y1, 1, 3] = 0;
+                    }
+                }
 
-                map[20, 34, 0] = 2;
-                map[20, 34, 1] = 0;
-                map[20, 34, 2] = 5;
+                for (int y1 = 0; y1 < 260; y1++)
+                {
+                    for (int x1 = 0; x1 < 260; x1++)
+                    {
+                        map[x1, y1, 2, 0] = rand.wh(new int[] { 1, 90 });
+                        if (map[x1, y1, 2, 0] == 0) map[x1, y1, 2, 1] = 1;
+                        else map[x1, y1, 2, 1] = rand.wh(new int[] { 0, 1, 6, 3 });
+                        map[x1, y1, 2, 2] = rand.wh(new int[] {0, 1, 5, 10, 35, 4, 1 });
+                        map[x1, y1, 2, 3] = 0;
+                    }
+                }
 
-                map[200, 43, 0] = 2;
-                map[200, 43, 1] = 1;
-                map[200, 43, 2] = 7;
+                for (int y1 = 0; y1 < 260; y1++)
+                {
+                    for (int x1 = 0; x1 < 260; x1++)
+                    {
+                        map[x1, y1, 3, 0] = rand.wh(new int[] { 1, 90 });
+                        if (map[x1, y1, 3, 0] == 0) map[x1, y1, 3, 1] = 1;
+                        else map[x1, y1, 3, 1] = rand.wh(new int[] { 0, 0, 1, 6, 3});
+                        map[x1, y1, 3, 2] = rand.wh(new int[] { 1, 0, 0, 1, 2, 10, 20, 38, 10 });
+                        map[x1, y1, 3, 3] = 0;
+                    }
+                }
 
-                map[32, 150, 0] = 2;
-                map[32, 150, 1] = 1;
-                map[32, 150, 2] = 8;
+                map[255, 1, 3, 0] = 0;
+                map[255, 1, 3, 1] = 0;
+                map[255, 1, 3, 2] = 9;
 
-                map[2, 21, 0] = 2;
-                map[2, 21, 1] = 1;
-                map[2, 21, 2] = 9;
+                map[20, 34, 3, 0] = 2;
+                map[20, 34, 3, 1] = 0;
+                map[20, 34, 3, 2] = 9;
+
+                map[200, 43, 3, 0] = 2;
+                map[200, 43, 3, 1] = 1;
+                map[200, 43, 3, 2] = 9;
+
+                map[32, 150, 3, 0] = 2;
+                map[32, 150, 3, 1] = 1;
+                map[32, 150, 3, 2] = 9;
+
+                map[2, 21, 3, 0] = 2;
+                map[2, 21, 3, 1] = 1;
+                map[2, 21, 3, 2] = 9;
 
             }
             else
@@ -90,172 +127,132 @@ namespace TBA_V2
                 for (int y1 = 0; y1 < 260; y1++)
                 {
                     for (int x1 = 0; x1 < 260; x1++)
-                    {
+                    {/*
                         map[x1, y1, 0] = mapl.Read() - '0';
                         map[x1, y1, 1] = mapl.Read() - '0';
                         map[x1, y1, 2] = mapl.Read() - '0';
                         map[x1, y1, 3] = mapl.Read() - '0';
 
+                    }*/
                     }
+                    mapl.ReadLine();
+                    y = int.Parse(mapl.ReadLine());
+                    x = int.Parse(mapl.ReadLine());
+                    lastx = int.Parse(mapl.ReadLine());
+                    lasty = int.Parse(mapl.ReadLine());
+                    mapl.Close();
                 }
-                mapl.ReadLine();
-                y = int.Parse(mapl.ReadLine());
-                x = int.Parse(mapl.ReadLine());
-                lastx = int.Parse(mapl.ReadLine());
-                lasty = int.Parse(mapl.ReadLine());
-                mapl.Close();
-            }
 
-        }
-        public void mapsave(string name)
-        {
-            StreamWriter Mps = new StreamWriter(name + ".map");
-            foreach (int i in map)
-            {
-                Mps.Write(i);
-            }
-            Mps.Write('\n');
-            Mps.WriteLine(y);
-            Mps.WriteLine(x);
-            Mps.WriteLine(lastx);
-            Mps.WriteLine(lasty);
-            Mps.Close();
-        }
-        public void North()
-        {
-            if (x != 0)
-            {
-                if (map[x - 1, y, 0] == 2)
-                {
-                    Program.player.ill++;
-                }
-                else
-                {
-                    lastx = x;
-                    x--;
-                    map[x, y, 3] = 1;
-                    Program.player.spll++;
-                }
-            }
-            else
-            {
-                Program.player.hp--;
-                Program.player.ill++;
-            }
-
-        }
-        public void South()
-        {
-            if (x != 255)
-            {
-                if (map[x + 1, y, 0] == 2)
-                {
-                    Program.player.ill++;
-                }
-                else
-                {
-                    lastx = x;
-                    x++;
-                    map[x, y, 3] = 1;
-                    Program.player.spll++;
-                }
-            }
-            else
-            {
-                Program.player.hp--;
-                Program.player.ill++;
             }
         }
-        public void East()
-        {
-            if (y != 255)
+            /*public void mapsave(string name)
             {
-                if (map[x, y + 1, 0] == 2)
+                StreamWriter Mps = new StreamWriter(name + ".map");
+                foreach (int i in map)
                 {
-                    Program.player.ill++;
+                    Mps.Write(i);
                 }
-                else
-                {
-                    lasty = y;
-                    y++;
-                    map[x, y, 3] = 1;
-                    Program.player.spll++;
-                }
+                Mps.Write('\n');
+                Mps.WriteLine(y);
+                Mps.WriteLine(x);
+                Mps.WriteLine(lastx);
+                Mps.WriteLine(lasty);
+                Mps.Close();
             }
-            else
+            public void North()
             {
-                Program.player.hp--;
-                Program.player.ill++;
-            }
-        }
-        public void West()
-        {
-            if (y != 0)
-            {
-                if (map[x, y - 1, 0] == 2)
+                if (x != 0)
                 {
-                    Program.player.ill++;
-                }
-                else
-                {
-                    lasty = y;
-                    y--;
-                    map[x, y, 3] = 1;
-                    Program.player.spll++;
-                }
-            }
-            else
-            {
-                Program.player.hp--;
-                Program.player.ill++;
-            }
-        }
-
-        public void reuturn()
-        {
-            x = lastx;
-            y = lasty;
-        }
-        public void show()
-        {
-            Console.Clear();
-            for (int y1 = 0; y1 < 260; y1++)
-            {
-                for (int x1 = 0; x1 < 260; x1++)
-                {
-                    if ((map[x1, y1, 3]) == 0) IO.O("■");
-                    else if (x1 == x && y1 == y) IO.O("▣");
+                    if (map[x - 1, y, 0] == 2)
+                    {
+                        Program.player.ill++;
+                    }
                     else
                     {
-                        switch (map[x1, y1, 1])
-                        {
-                            case 0:
-                                if ((map[x1, y1, 1]) == 0) IO.O("▩");
-                                else if ((map[x1, y1, 2]) == 0) IO.O("□");
-                                else
-                                {
-                                    Console.ForegroundColor = ConsoleColor.Yellow;
-                                    IO.O("▦");
-                                    Console.ForegroundColor = ConsoleColor.White;
-                                }
-                                break;
-                            case 1:
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                IO.O("▧");
-                                Console.ForegroundColor = ConsoleColor.White;
-                                break;
-                            default:
-                                Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                                IO.O("▧");
-                                Console.ForegroundColor = ConsoleColor.White;
-                                break;
-
-                        }
+                        lastx = x;
+                        x--;
+                        map[x, y, 3] = 1;
+                        Program.player.spll++;
                     }
                 }
-                IO.O("\n");
+                else
+                {
+                    Program.player.hp--;
+                    Program.player.ill++;
+                }
+
             }
-            Console.Clear();
-        }
+            public void South()
+            {
+                if (x != 255)
+                {
+                    if (map[x + 1, y, 0] == 2)
+                    {
+                        Program.player.ill++;
+                    }
+                    else
+                    {
+                        lastx = x;
+                        x++;
+                        map[x, y, 3] = 1;
+                        Program.player.spll++;
+                    }
+                }
+                else
+                {
+                    Program.player.hp--;
+                    Program.player.ill++;
+                }
+            }
+            public void East()
+            {
+                if (y != 255)
+                {
+                    if (map[x, y + 1, 0] == 2)
+                    {
+                        Program.player.ill++;
+                    }
+                    else
+                    {
+                        lasty = y;
+                        y++;
+                        map[x, y, 3] = 1;
+                        Program.player.spll++;
+                    }
+                }
+                else
+                {
+                    Program.player.hp--;
+                    Program.player.ill++;
+                }
+            }
+            public void West()
+            {
+                if (y != 0)
+                {
+                    if (map[x, y - 1, 0] == 2)
+                    {
+                        Program.player.ill++;
+                    }
+                    else
+                    {
+                        lasty = y;
+                        y--;
+                        map[x, y, 3] = 1;
+                        Program.player.spll++;
+                    }
+                }
+                else
+                {
+                    Program.player.hp--;
+                    Program.player.ill++;
+                }
+            }
+
+            public void reuturn()
+            {
+                x = lastx;
+                y = lasty;
+            }*/
     }
 }
