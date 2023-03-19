2df236234b42ca0277b6afba23bcf708becd2f79
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2023-03-19 17:44:23 +0100

    Update Program.cs

--- TBA-V2/Program.cs
+++ TBA-V2/Program.cs
@@ -19,103 +19,16 @@ namespace TBA_V2
             move.mapgen("¦");
             IO.Refresh("");
             IO.Map();
-            Console.ReadKey();
-            /*IO.O("do you want to load a saved game? (y/n)\n");
-            if (Console.ReadLine() == "y")
+            IO.Load();
+            IO.Refresh("");
+            IO.Map();
+            while (true)
             {
-                Console.Clear();
-                IO.O("input name of saved character\n");
-                player.Chload(IO.Itxt());
-                inv.invgen(player.name);
-                move.mapgen(player.name);
+                IO.Wait();
+                IO.Menu();
+                IO.Refresh("");
+                IO.Map();
             }
-            else
-            {
-                Console.Clear();
-                player.name = IO.IOtxt("I herby declare that i ", " will serve the eternal guild with my soul untill i am released, i also am aware that if i shuld fall during the completion of a quest my mind and soul will be retured to the time and place i was issued that quest.");
-                if (File.Exists(player.name + ".save"))
-                {
-                    Console.Clear();
-                    player.name = IO.IOtxt("Name alredy exists, if you want to override it please input the same name again, otherwise type another name.\nWe will not warn you again if this name exists, please check beforehand: ");
-                }
-                Console.Read();
-                player.Chgen();
-                inv.invgen("¦");
-                move.mapgen("¦");
-            }
-            while (player.alive)
-            {
-                switch (move.map[move.x, move.y, 0])
-                {
-                    case 0:
-                        Console.Clear();
-                        IO.O("do you want to \"save\"");
-                        if (move.map[move.y, move.x, 2] > 0) IO.O(", \"loot\" the room");
-                        IO.O(", or go \"north\", \"west\", \"south\", or \"east\"?\n");
-                        switch (IO.Itxt())
-                        {
-                            case "save":
-                                Console.Clear();
-                                player.Chsave();
-                                move.mapsave(player.name);
-                                inv.invsave(player.name);
-                                IO.O("Game saved sucsessfully");
-                                Console.Read();
-                                break;
-                            case "loot":
-                                if (move.map[move.y, move.x, 2] > 0)
-                                {
-                                    Console.Clear();
-                                    inv.loot();
-                                }
-                                else player.ill++;
-                                break;
-                            case "north":
-                                move.North();
-                                break;
-                            case "west":
-                                move.West();
-                                break;
-                            case "south":
-                                move.South();
-                                break;
-                            case "east":
-                                move.East();
-                                break;
-                            case "map":
-                                move.show();
-                                break;
-                            default:
-                                player.ill++;
-                                break;
-                        }
-                        break;
-                    case 2:
-                        //bossgen
-                        break;
-                    case 1:
-                        figt.mongen(move.map[move.x, move.y, 1]);
-                        switch (figt.battle())
-                        {
-                            case true:
-                                move.map[move.x, move.y, 0] = 0;
-                                move.map[move.x, move.y, 1] = 1;
-                                break;
-                            case false:
-                                move.reuturn();
-                                break;
-                            case null:
-                                player.alive = false;
-                                IO.O("you died");
-                                break;
-                        }
-                        
-                        break;
-                    default:
-                        break;
-                }
-            }
-            */
         }
     }
 }
