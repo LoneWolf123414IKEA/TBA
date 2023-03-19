88f581952ad5c01c42a9c844faafff2a18856c89
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2023-03-19 23:45:53 +0100

    Add files via upload

---
+++ TBA-V2/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBA_V2
+{
+    internal class Item
+    {
+        public bool[] equipsin = new bool[33];
+        public bool[] equipneed = new bool[14];
+        public static void Invuse(Item item)
+        {
+
+        }
+    }
+}
